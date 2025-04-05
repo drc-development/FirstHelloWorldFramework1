bde4f18a61cf6b53149298ab9130f5e0b2ab756a
DarylRCox1956 <dcox@justmacapps.net>
2025-04-05 12:30:47 -0500

    Initial Commit

---
+++ FirstHelloWorldFramework1/FirstHelloWorldFramework1.swift
@@ -0,0 +1,9 @@
+//
+//  FirstHelloWorldFramework1.swift
+//  FirstHelloWorldFramework1
+//
+//  Created by Daryl Cox on 4/5/25.
+//
+
+import Foundation
+
---
+++ FirstHelloWorldFramework1Tests/FirstHelloWorldFramework1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstHelloWorldFramework1Tests.swift
+//  FirstHelloWorldFramework1Tests
+//
+//  Created by Daryl Cox on 4/5/25.
+//
+
+import Testing
+@testable import FirstHelloWorldFramework1
+
+struct FirstHelloWorldFramework1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
