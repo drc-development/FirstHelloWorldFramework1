78bd47de55d79ade47b9eae74bca558f3b36d5fa
DarylRCox1956 <dcox@justmacapps.net>
2025-04-06 15:37:40 -0500

    Initial commit before push to GitHub

--- FirstHelloWorldFramework1/FirstHelloWorldFramework1.swift
+++ FirstHelloWorldFramework1/FirstHelloWorldFramework1.swift
@@ -7,3 +7,20 @@
 
 import Foundation
 
+public struct FirstHelloWorldFramework1
+{
+    
+    public init()
+    {
+        
+    }   // End of public init().
+    
+    public func sayHello(to name:String)->String
+    {
+        
+        return "Hello, \(name)!"
+        
+    }   // End of public func sayHello(to name:String)->String.
+    
+}   // End of public struct FirstHelloWorldFramework1.
+
--- FirstHelloWorldFramework1Tests/FirstHelloWorldFramework1Tests.swift
+++ FirstHelloWorldFramework1Tests/FirstHelloWorldFramework1Tests.swift
@@ -6,12 +6,35 @@
 //
 
 import Testing
-@testable import FirstHelloWorldFramework1
+@testable
+import FirstHelloWorldFramework1
 
-struct FirstHelloWorldFramework1Tests {
+struct FirstHelloWorldFramework1Tests
+{
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+    @Test("Test FirstHelloWorldFramework1 'Greeting' Functionality")
+    func testGreeting()async throws
+    {
+        
+        let sCurrMethod:String = #function
+        let sCurrMethodDisp    = "FirstHelloWorldFramework1Tests.'"+sCurrMethod+"':"
+        
+        print("\(sCurrMethodDisp) Invoked...")
+        
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions...
+        
+        let firstHelloWorldFramework1:FirstHelloWorldFramework1 = FirstHelloWorldFramework1()
+        let sfirstHelloWorldFramework1Greeting:String           = firstHelloWorldFramework1.sayHello(to:"World")
+        
+        print("\(sCurrMethodDisp) Intermediate 'sfirstHelloWorldFramework1Greeting' is [\(sfirstHelloWorldFramework1Greeting)]...")
+        
+        #expect(sfirstHelloWorldFramework1Greeting == "Hello, World!")
+        
+        print("\(sCurrMethodDisp) Exiting...")
+        
+        return
+        
+    }   // End of @Test func testGreeting()async throws.
 
-}
+}   // End of struct FirstHelloWorldFramework1Tests.
+
